d4a6c3033bb2fb3b2424c6f0c7b3c050199d7174
Christian Brel <ch.brel@gmail.com>
2015-10-06 17:34:54 +0200

    refs SERVICES-117 : Fix bug in GuestBookClientNamespaceManager : use property of a JSON object...

--- scripts/GuestBookClientNamespaceManager.ts
+++ scripts/GuestBookClientNamespaceManager.ts
@@ -35,10 +35,10 @@ class GuestBookClientNamespaceManager extends NamespaceManager {
 	takeControl(callSocketId : any) {
 		var self = this;
 
-		var callNamespaceManager = self.server().retrieveNamespaceManagerFromSocketId(callSocketId);
+		var callNamespaceManager = self.server().retrieveNamespaceManagerFromSocketId(callSocketId.callSocketId);
 
 		if(callNamespaceManager == null) {
-			self.socket.emit("ControlSession", self.formatResponse(false, "NamespaceManager corresponding to callSocketid '" + callSocketId + "' doesn't exist."));
+			self.socket.emit("ControlSession", self.formatResponse(false, "NamespaceManager corresponding to callSocketid '" + callSocketId.callSocketId + "' doesn't exist."));
 		} else {
 
 			var newSession : Session = callNamespaceManager.newSession(self);
